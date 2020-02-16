23c2c2a1d7cff4975d5bc948f468b45b25c3d4ef
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-02-16 19:03:36 +0100

    Kangaroo exercise

---
+++ src/Kangaroo.java
@@ -0,0 +1,52 @@
+import javax.swing.plaf.synth.SynthMenuBarUI;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        int kangaroo1Position = x1;
+        int kangaroo2Position = x2;
+
+        while((v1 > v2 && kangaroo1Position < kangaroo2Position) || (v2>v1 && kangaroo2Position < kangaroo1Position)) {
+            kangaroo1Position +=v1;
+            kangaroo2Position+=v2;
+        }
+        if(kangaroo1Position==kangaroo2Position){
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
