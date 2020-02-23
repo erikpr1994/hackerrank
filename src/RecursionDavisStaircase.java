613343b6117dc6e8be403fd007dbdfc9bbc37d16
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-02-23 14:45:43 +0100

    Refactoring of recursion Davi's Starcase without Memoization
    Refactoring of recursion Davi's Starcase without Memoization

--- src/RecursionDavisStaircase.java
+++ src/RecursionDavisStaircase.java
@@ -1,38 +1,77 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Solution {
 
     // Complete the stepPerms function below.
     static int stepPerms(int n) {
-        int [] number = new int [n+1];
+        int[] number = new int[n + 1];
         number[0] = 1;
-        return calcs(n,number);
+        return calcs(n, number);
     }
 
-    private static int calcs(int n, int[] number){
+    private static int calcs(int n, int[] number) {
         // Cuando la N es más pequeño que 0, devuelve 0
-        if(n<0){
+        if (n < 0) {
             return 0;
         }
+
         // si la posicion de number n es diferente a 0, devuelve number y su posicion
-        if(number[n]!=0){
+        if (number[n] != 0) {
             return number[n];
         }
 
         // si n es 1, devuelve 1
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
 
         // Calcula la posicion 0 de number
-        number[n]= calcs(n-3,number) + calcs(n-2,number) + calcs(n-1,number);
+        number[n] = calcs(n - 3, number) + calcs(n - 2, number) + calcs(n - 1, number);
         return number[n];
+
+
+        /*// Complete the stepPerms function below.
+        static int stepPerms(int n) {
+            int result = 0;
+            if(n<=0){
+                result = 0;
+                System.out.println(n + " -> " + result);
+                return 0;
+            }
+            if(n==1){
+                result = 1;
+                System.out.println(n + " -> " + result);
+                return 1;
+            }
+
+            if(n==3){
+                result = 4;
+                System.out.println(n + " -> " + result);
+                return result;
+            }
+
+            result = stepPerms(n-1)+1;
+            System.out.println(n + " -> " + result);
+            return result;
+        }
+
+        static int stepPerms2(int n) {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+
+            int stepsM3 = stepPerms2(n - 3);
+            if (stepsM3 > 0) stepsM3 += stepPerms2(3);
+
+            int stepsM2 = stepPerms2(n - 2);
+            if (stepsM2 > 0) stepsM2 += stepPerms2(2);
+
+            int stepsM1 = stepPerms2(n - 1);
+            if (stepsM1 > 0) stepsM1 += 1;
+            return stepsM3 + stepsM2 + stepsM1;
+        }*/
     }
 
     private static final Scanner scanner = new Scanner(System.in);
