c89a73b73ca03300f0e804def7d5f406e8962562
Erik <erikpastorrios1994@gmail.com>
2020-04-11 19:20:31 +0200

    DynamicArray exercise

---
+++ src/DynamicArray.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        int lastNumber = 0;
+        List<Integer> result = new ArrayList<>();
+        List<Integer>[] arr = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (List<Integer> query : queries) {
+            int number0 = query.get(0);
+            int number1 = query.get(1);
+            int number2 = query.get(2);
+            if (number0 == 1) {
+                arr[(number1 ^ lastNumber) % n].add(number2);
+            } else {
+                lastNumber = arr[((number1 ^ lastNumber) % n)].get(number2 % arr[((number1 ^ lastNumber) % n)].size());
+                result.add(lastNumber);
+            }
+        }
+        return result;
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, q).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.dynamicArray(n, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
